3759fbb0f5473d56f7ac606c7e54626e0d690114
Gábor Kovács <gabor950601@gmail.com>
2022-02-10 17:28:26 +0100

    add tailwind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -13,4 +12,4 @@ createApp(App)
 .use(router)
 .mount('#app')
 
-import "bootstrap/dist/js/bootstrap.js"
+import './assets/tailwind.css'
