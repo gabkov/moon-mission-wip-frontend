6e70ef55f68108dc7b8462ea12c1de8b72a60856
Gábor Kovács <gabor950601@gmail.com>
2022-02-23 17:21:06 +0100

    public folder for tokens

--- src/components/farmcard/FarmRow.vue
+++ src/components/farmcard/FarmRow.vue
@@ -15,7 +15,7 @@
         <td class="px-4 py-5">
             <div class="flex items-center text-sm">
                 <div class="w-9 h-9 mr-3 rounded-full">
-                    <img v-if="single" class="w-16" :src="logo" alt="">
+                    <img v-if="single" class="w-16" :src="'/tokens/' + logo + '.png'" :alt="logo">
                     <div v-else class="relative">
                         <img class="w-8 h-8 absolute top-0 -left-1 " src="https://cronosapp.cougarswap.io/images/single-token/WBTC.png" alt="">
                         <img class="w-8 h-8 absolute top-3 left-2" src="@/assets/tokens/fuel_icon.png" alt="fuel">
